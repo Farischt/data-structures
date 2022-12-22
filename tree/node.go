df12cbff0f0b22f5b5331ac0cf40ba676d38bfa9
Faris CHTATOU <faris.manage@gmail.com>
2022-12-22 23:39:36 +0100

    add has child method

--- tree/node.go
+++ tree/node.go
@@ -1,11 +1,15 @@
 package tree
 
 type Node struct {
-	data int
-	left *Node
+	data  int
+	left  *Node
 	right *Node
 }
 
 func NewNode(data int) *Node {
 	return &Node{data: data}
+}
+
+func (n *Node) HasChild() bool {
+	return n.left != nil || n.right != nil
 }

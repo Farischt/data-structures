2c98bff6bb4c862ab6a2e525411c0aa54ad84b4b
Faris CHTATOU <faris.manage@gmail.com>
2022-12-24 23:34:19 +0100

    add queue testing

---
+++ queue/queue_test.go
@@ -0,0 +1,66 @@
+package queue
+
+import "testing"
+
+func TestIsEmpy(t *testing.T) {
+	q := New[int](1)
+	if !q.IsEmpty() {
+		t.Errorf("Expected queue to be empty")
+	}
+}
+
+func TestIsFull(t *testing.T) {
+	q := New[int](1)
+	q.Enqueue(10)
+	if !q.IsFull() {
+		t.Errorf("Expected queue to be full")
+	}
+}
+
+func TestEnqueue(t *testing.T) {
+	q := New[int](2)
+	q.Enqueue(10)
+	q.Enqueue(20)
+	if !q.IsFull() {
+		t.Errorf("Expected queue to be full")
+	}
+}
+
+func TestEnqueueFullQueue(t *testing.T) {
+	q := New[int](1)
+	q.Enqueue(10)
+	err := q.Enqueue(20)
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
+
+func TestDequeue(t *testing.T) {
+	expectedDequeuedValue := 10
+	q := New[int](2)
+	q.Enqueue(expectedDequeuedValue)
+	q.Enqueue(20)
+	value, err := q.Dequeue()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	} else if *value != expectedDequeuedValue {
+		t.Errorf("Expected %d, got %d", expectedDequeuedValue, *value)
+	}
+}
+
+func TestDequeueEmptyQueue(t *testing.T) {
+	q := New[int](1)
+	_, err := q.Dequeue()
+	if err == nil {
+		t.Errorf("Expected dequeue to return error, got nil")
+	}
+}
+
+func TestSize(t *testing.T) {
+	q := New[int](2)
+	q.Enqueue(10)
+	q.Enqueue(20)
+	if q.Size() != 2 {
+		t.Errorf("Expected size 2, got %d", q.Size())
+	}
+}
